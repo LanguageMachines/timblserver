71af3619552d89e1adaec2f1e49ed87605ff7cf2
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-01-02 09:34:21 +0100

    2018

--- src/TimblServer.cxx
+++ src/TimblServer.cxx
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 1998 - 2017
+  Copyright (c) 1998 - 2018
   CLST  - Radboud University
   ILK   - Tilburg University
   CLiPS - University of Antwerp
@@ -677,7 +677,7 @@ int main(int argc, char *argv[]){
     // Start.
     //
     cerr << "TiMBL Server " << TimblServer::Version()
-	 << " (c) CLTS/ILK/CLIPS 1998 - 2017.\n"
+	 << " (c) CLTS/ILK/CLIPS 1998 - 2018.\n"
 	 << "Tilburg Memory Based Learner\n"
 	 << "Centre for Language and Speech Technology, Radboud University\n"
 	 << "Induction of Linguistic Knowledge Research Group, Tilburg University\n"
